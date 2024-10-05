33ab523e8d109236d05dd6f55a715541ca2f5099
Pedro Baptista <pedrolk2012@gmail.com>
2024-10-05 15:55:27 -0300

    wip

--- config/database.ts
+++ config/database.ts
@@ -3,16 +3,6 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || "your_database_name", // DB Name
-  process.env.DB_USER || "admin", // DB User
-  process.env.DB_PASSWORD || "password", // DB Password
-  {
-    host: process.env.DB_HOST || "localhost", // Docker container runs on localhost
-    port: Number(process.env.DB_PORT) || 5432, // PostgreSQL default port
-    dialect: "postgres", // Specify the database dialect
-    logging: false, // Disable logging for cleaner output
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL as string);
 
 export default sequelize;
